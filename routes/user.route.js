b9368eed663dbff49544137c3ce985188d871607
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-21 08:54:27 +0100

    chore: Update user registration route to use POST method

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { createUser } = require("../controller/user.controller.js");
 
-router("/register", createUser);
+router.post("/register", createUser);
 
 module.exports = router;
