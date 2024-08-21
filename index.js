e0df15878dfaa378b65531eb993876f69c25fdb8
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-21 06:53:28 +0100

    chore: Connect to MongoDB database

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+process.loadEnvFile(".env");
 
 const app = express();
 app.use(express.json());
 
 app.get("/", () => console.log("Server Running in Port 3000"));
 app.listen(3000);
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Database is connected Successfully"))
+  .catch((error) => console.log(error.message));
