c6e3757d81226295fde1b0c9614b085830210bf9
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-21 07:17:57 +0100

    chore: Add user model with required fields

---
+++ models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    mail: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
