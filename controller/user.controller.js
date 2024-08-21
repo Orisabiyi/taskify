375f4f337c244ab4a44dec6129a80c56855f73fa
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-21 08:30:42 +0100

    chore: Sanitize user input, normalize email, and hash password in createUser function

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -5,20 +5,24 @@ const validateUserRegisterInput = require("../utils/validate.utils.js");
 const User = require("../models/user.model.js");
 
 const createUser = async function (req, res) {
-  let { name, email, password } = req.body;
+  try {
+    let { name, email, password } = req.body;
 
-  // sanitize inputs
-  name = validator.escape(name);
-  email = validator.normalizeEmail(email);
-  password = validator.escape(password);
+    // sanitize inputs
+    name = validator.escape(name);
+    email = validator.normalizeEmail(email);
+    password = validator.escape(password);
 
-  if (await User.findOne({ name }))
-    return res
-      .status(409)
-      .json({ message: "A user with that name already exists" });
+    if (await User.findOne({ name }))
+      return res
+        .status(409)
+        .json({ message: "A user with that name already exists" });
 
-  validateUserRegisterInput(email, password);
+    validateUserRegisterInput(email, password);
 
-  // email = email.toLowerCase();
-  // password = bcrypt.hash(password, 10);
+    email = email.toLowerCase();
+    password = bcrypt.hash(password, 10);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
