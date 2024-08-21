d9c89110514415d2925cae0191c20645acf030dc
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-21 09:05:42 +0100

    chore: Update email validation in validateUserRegisterInput

--- utils/validate.utils.js
+++ utils/validate.utils.js
@@ -2,22 +2,24 @@ const validator = require("validator");
 
 const validateUserRegisterInput = function (res, email, password) {
   if (!validator.isEmail(email))
-    return res.status(409).json({ message: "The email provided is not valid" });
+    return res.status(400).json({ message: "The email provided is not valid" });
 
   if (!validator.isLength(password, { min: 8 }))
     return res
-      .status(409)
+      .status(400)
       .json({ message: "Password must be at least 8 characters" });
 
   if (!validator.isAlphanumeric(password))
-    res
-      .status(409)
+    return res
+      .status(400)
       .json({ message: "Password must contain only numbers and letters" });
 
   if (!/[!@#$%^&*(),.?":{}|<>]/.test(password))
-    return res.status(409).json({
+    return res.status(400).json({
       message: "Password must contain at least one special character",
     });
+
+  return true;
 };
 
 module.exports = validateUserRegisterInput;
