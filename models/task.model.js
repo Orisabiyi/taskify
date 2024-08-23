467949cfac8c8c8059ce3c3efe5192d9cc7c5f42
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-23 10:46:49 +0100

    chore: include userId in the task table

--- models/task.model.js
+++ models/task.model.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const TaskSchema = mongoose.Schema(
   {
+    userId: {
+      type: Number,
+      required: true,
+    },
     name: {
       type: String,
       required: true,
