7f0c1ee2024a201513fe11f3ddc588167f1797a3
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-23 11:29:17 +0100

    refactor: refactor task controller codebase for validation

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -1,40 +1,21 @@
-const Task = require("../models/task.model.js");
 const validator = require("validator");
+const { taskInputValidator } = require("../utils/validate.utils.js");
+const Task = require("../models/task.model.js");
 
 const createTask = async function (req, res) {
   try {
     const { userId, name, status, categories, priority, dueDate } = req.body;
 
-    // validate user inputs
-    if (!userId || typeof userId !== Number)
-      return res.status(401).json({ message: "invalid userId" });
-
-    if (!name || typeof name !== String)
-      return res.status(401).json({ message: "invalid name" });
-
-    if (
-      !status ||
-      status.toLowerCase() !== "complete" ||
-      status.toLowerCase() !== "uncomplete"
-    )
-      res.status(401).json({ message: "invalid status value" });
-
-    if (!categories || typeof categories !== String)
-      res.status(401).json({
-        message: !categories
-          ? "provide category value"
-          : "categories value should be a string",
-      });
-
-    if (!priority || typeof priority !== Number)
-      res.status(401).json({
-        message: !priority
-          ? "provide a value for priority"
-          : "priority should be a number",
-      });
-
-    if (!validator.isDate(dueDate))
-      res.status(401).json({ message: "date is invalid" });
+    // if (taskInputValidator()) return
+    taskInputValidator(
+      res,
+      userId,
+      name,
+      status,
+      categories,
+      priority,
+      dueDate
+    );
   } catch (error) {
     res.status(500).json({ messae: error.message });
   }
