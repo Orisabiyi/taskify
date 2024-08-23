ab90ad3d44c0c0cec27095a1efa7dbb9a1ac8fe7
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-23 03:20:04 +0100

    chore: refactor codebase

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -4,11 +4,10 @@ const authenticateToken = function (req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
+  if (!token)
     return res
       .status(401)
       .json({ message: "Access token is missing or invalid" });
-  }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
