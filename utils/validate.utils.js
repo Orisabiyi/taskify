b6c5e9065be4ba059502268f09d76b5f06782c94
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-23 12:59:50 +0100

    refactor: Fix validation issues in task controller and utils

--- utils/validate.utils.js
+++ utils/validate.utils.js
@@ -37,7 +37,7 @@ const taskInputValidator = function (
   dueDate
 ) {
   // validate user inputs
-  if (!userId || typeof userId !== Number)
+  if (!userId || typeof userId !== String)
     return res.status(401).json({ message: "invalid userId" });
 
   if (!name || typeof name !== String)
