72f8dba90c9681135f2aca4335148d442b76895d
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-31 13:36:18 +0100

    refactor: Remove unnecessary code and improve error handling in task controller

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -45,7 +45,6 @@ const getAllTasks = async function (req, res) {
       return res
         .status(404)
         .json({ message: "Create a tasks, your task list is empty" });
-
     res.status(200).json({ tasks });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -55,6 +54,7 @@ const getAllTasks = async function (req, res) {
 const filterTaskByCategory = async function (req, res) {
   try {
     const { category } = req.body;
+
     if (!category)
       return res.status(400).json({ message: "Category is required" });
     if (typeof category !== "string")
