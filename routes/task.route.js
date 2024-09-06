205d34b7d61e4b4fa2bbcc9e287825b233785f4e
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-06 18:26:50 +0100

    refactor: Reorder routes in task.route.js for better readability

--- routes/task.route.js
+++ routes/task.route.js
@@ -12,12 +12,13 @@ const {
 } = require("../controller/task.controller.js");
 
 router.get("/", getUserTasks);
-router.post("/create", createUserTasks);
-router.post("/update", updateUserTask);
 router.post("/delete", deleteTask);
+router.post("/update", updateUserTask);
+router.post("/create", createUserTasks);
+
 router.post("/filter/category", filterTaskByCategory);
+router.post("/filter/priority", filterTaskByPriority);
 router.post("/filter/status", filterTaskByStatus);
 router.post("/filter/date", filterTaskByDate);
-router.post("/filter/priority", filterTaskByPriority);
 
 module.exports = router;
