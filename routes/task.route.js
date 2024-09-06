dfda59c6f7f3c49ae5eb30d471f83b2881c6227c
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-06 18:22:43 +0100

    refactor: Update route paths for user and task APIs

--- routes/task.route.js
+++ routes/task.route.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const {
-  createTask,
-  getAllTasks,
+  createUserTasks,
+  getUserTasks,
   filterTaskByStatus,
   filterTaskByCategory,
   filterTaskByDate,
   filterTaskByPriority,
 } = require("../controller/task.controller.js");
 
-router.get("/", getAllTasks);
-router.post("/create", createTask);
+router.get("/", getUserTasks);
+router.post("/create", createUserTasks);
 router.post("/filter/category", filterTaskByCategory);
 router.post("/filter/status", filterTaskByStatus);
 router.post("/filter/date", filterTaskByDate);
